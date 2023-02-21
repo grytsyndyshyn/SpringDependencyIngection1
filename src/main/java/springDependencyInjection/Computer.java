3206035bfdbbaeeaacfe769d43feeb1275216a89
Grytsyndyshyn <grytsyndyshyn@gmail.com>
2023-02-21 08:41:11 +0200

    DependencyInjection1

---
+++ src/main/java/springDependencyInjection/Computer.java
@@ -0,0 +1,15 @@
+package springDependencyInjection;
+
+public class Computer {
+    private int id;
+    private MusicPlayer musicPlayer;
+    public Computer(MusicPlayer musicPlayer) {
+        this.id=1;
+        this.musicPlayer = musicPlayer;
+    }
+
+    @Override
+    public String toString() {
+        return "Computer " + id + " " + musicPlayer.playMusic();
+    }
+}
